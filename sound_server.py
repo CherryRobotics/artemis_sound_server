0aa34cc77d5d43bbc9559744a731a6cd737f101d
KadynCBR <kadyn_martinez@hotmail.com>
2017-02-26 17:23:26 -0800

    iforgotwhatichanged._.

--- sound_server.py
+++ sound_server.py
@@ -1,18 +1,33 @@
 import json
 from utils.SWRAP import SWRAP
 import logging
+import threading
 from watson_developer_cloud import SpeechToTextV1
 logging.basicConfig(level=logging.INFO,
                     format='[%(asctime)s][%(name)s][%(levelname)s] %(message)s',
                     datefmt='%s',)
 log = logging.getLogger(__name__)
 
+class WatsonPrediction(object):
+    def __init__(self, resultjson=None):
+        self.word_confidences = None
+        self.confidence = None
+        self.transcript = None
+        self.timestamps = None
+        if resultjson:
+            if len(resultjson["results"]) > 0:
+                alts = resultjson["results"][0]["alternatives"][0]
+                self.word_confidences = alts["word_confidence"]
+                self.confidence = alts["confidence"]
+                self.transcript = alts["transcript"]
+                self.timestamps = alts["timestamps"]
+
 class sound_server(object):
     def __init__(self):
-        self.swrap = SWRAP(mode=SWRAP.STREAM)
+        self.swrap = SWRAP(threshold=4  000, mode=SWRAP.STREAM)
         self.swrap.stream_start()
         self.get_key_and_pass()
-        self.result = {}
+        self.result = WatsonPrediction()
         self.watsonstt = SpeechToTextV1(
             username=self.username,
             password=self.password,
@@ -32,25 +47,25 @@ class sound_server(object):
     ##### SpeechToText #####
     def parse_voice(self, voice):
         result = self.watsonstt.recognize(
-            voice, content_type='audio/wav', timestamps=False,
+            voice, content_type='audio/wav', timestamps=True,
             word_confidence=True
         )
-        self.result = result
-        # print self.result
-        if len(self.result["results"]) >= 1:
-            print self.result["results"][0]["alternatives"][0]["transcript"]
-        else:
-            print "No result"
+        self.result = WatsonPrediction(result)
 
     def waitforwav(self):
-        log.info("Searching for voice")
-        wav = self.swrap.listen()
-        log.info("Voice found!")
-        self.parse_voice(wav)
+        while True:
+            log.info("Searching for voice")
+            wav = self.swrap.listen()
+            log.info("Voice found!")
+            self.parse_voice(wav)
 
     def spin(self):
+        thread = threading.Thread(target=self.waitforwav)
+        thread.start()
         while True:
-            self.waitforwav()
+            if self.result.transcript:
+                print self.result.transcript
+                self.result = WatsonPrediction()
 
 
 if __name__ == '__main__':
